25c8170db555205e8c5704d88f3e8e0fcdecb6ca
Ahmed-Qureshi-dev <ahmedqureshi6120@gmail.com>
2026-03-05 13:50:17 +0500

    first commit

---
+++ firebase.js
@@ -0,0 +1,48 @@
+
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  import { getAuth ,createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCmTPTs59bdQoig-PNqHPF_6Ee_UgiNUwA",
+    authDomain: "practice-d59b6.firebaseapp.com",
+    projectId: "practice-d59b6",
+    storageBucket: "practice-d59b6.firebasestorage.app",
+    messagingSenderId: "572653881738",
+    appId: "1:572653881738:web:8784ac48783c3386a5a073"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+ console.log("initialize", app);
+  const auth = getAuth(app)
+    console.log("auth", auth);
+
+    
+ // signup 
+var signupbtn = document.getElementById("signupbtn")
+signupbtn.addEventListener("click" ,signup)
+
+function signup(){
+  var semail = document.getElementById("semail").value
+  var spassword = document.getElementById("spassword").value
+  
+  createUserWithEmailAndPassword(auth, semail, spassword)
+  .then((userCredential) => {
+ 
+  const user = userCredential.user;
+  })
+  .catch((error) => {
+  const errorCode = error.code;
+  const errorMessage = error.message;
+ 
+  });
+}
+   
+
+
+
+
+
+
+
